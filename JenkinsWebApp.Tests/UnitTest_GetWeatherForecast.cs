1828c2f429dd9ee4f1a0d27f85c455e2969795e3
Prabhakar Changala <prabha.uu3@gmail.com>
2023-04-22 06:11:42 +0530

    added empty test-cases

---
+++ JenkinsWebApp.Tests/UnitTest_GetWeatherForecast.cs
@@ -0,0 +1,11 @@
+namespace JenkinsWebApp.Tests;
+
+public class UnitTest_GetWeatherForecast
+{
+    [Fact]
+    public void Get_UnitTest_GetWeatherForecast()
+    {
+        bool result = true;
+        Assert.False(result, "1 should not be prime");
+    }
+}
